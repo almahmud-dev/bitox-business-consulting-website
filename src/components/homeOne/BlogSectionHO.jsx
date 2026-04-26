1b98e8cf2db27dbfa9dda70ea73e74e5fc047047
basudevkumar <jhulonkumar40@gmail.com>
2026-04-26 20:20:00 +0600

    feat: added responsive in BlogSectionHO   and BlogCard component

--- src/components/homeOne/BlogSectionHO.jsx
+++ src/components/homeOne/BlogSectionHO.jsx
@@ -6,7 +6,7 @@ import { blogData } from "../helper/homeOnehelper";
 
 const BlogSectionHO = () => {
   return (
-    <section className="py-[120px]">
+    <section className=" py-[70px] lg:py-[120px]">
       <Container size={"lg"}>
         <h2 className="max-w-[800px] text-center mx-auto font-bold text-primary headingTwo">
           Insights & Updates: Driving Your Success
@@ -27,7 +27,7 @@ const BlogSectionHO = () => {
             })}
           </Responsive.Grid>
         </div>
-        <div className="mt-[60px]">
+        <div className=" mt-[20px] md:mt-[30px] lg::mt-[60px]">
           <button className="w-full cursor-pointer text-center py-5  text-primary font-medium text-base border border-[#0000001a] rounded-[6px  ]">
             See all case studies
           </button>
