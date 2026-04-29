bf5945046587fde414ad3bdcc5fc1c26a0ed55ad
basudevkumar <jhulonkumar40@gmail.com>
2026-04-30 00:57:22 +0600

    feat: added PricingPlan resuable component added in services page

--- src/app/(pages)/services/page.jsx
+++ src/app/(pages)/services/page.jsx
@@ -1,12 +1,15 @@
-import InnerPageHero from '@/components/ui/InnerPageHero'
-import React from 'react'
+import InnerPageHero from "@/components/ui/InnerPageHero";
+import PricingPlan from "@/components/homeTwo/PricingPlan";
+  
+import React from "react";
 
 const Services = () => {
   return (
     <>
-     <InnerPageHero title='Trusted High Quality Services'/>
+      <InnerPageHero title="Trusted High Quality Services" />
+      <PricingPlan />
     </>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
