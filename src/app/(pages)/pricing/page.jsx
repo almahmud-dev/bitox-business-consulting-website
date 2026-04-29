c26301aacbcda4d2ed4b1cf85d9463373794ccf6
basudevkumar <jhulonkumar40@gmail.com>
2026-04-30 00:45:36 +0600

    feat: added InnerPageHero resuable component in  Pricing page

--- src/app/(pages)/pricing/page.jsx
+++ src/app/(pages)/pricing/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import InnerPageHero from "@/components/ui/InnerPageHero";
+import React from "react";
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <>
+      <InnerPageHero
+        title="Bitox for Complete Solutions"
+      />
+    </>
+  );
+};
 
-export default Pricing
+export default Pricing;
--- src/components/teamMember/TeamIntro.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TeamIntro = () => {
-  return (
-    <div>TeamIntro</div>
-  )
-}
-
-export default TeamIntro
