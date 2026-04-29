b920bee40f8f1848e25d2f994afced696851ea50
basudevkumar <jhulonkumar40@gmail.com>
2026-04-30 00:05:22 +0600

    added InnerPageHero component in team page and in InnerPageHero component include new props titleWidth

--- src/app/(pages)/team/page.jsx
+++ src/app/(pages)/team/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import InnerPageHero from "@/components/ui/InnerPageHero";
+import React from "react";
 
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <>
+      <InnerPageHero
+        title="Meet Our Experienced Professionals"
+        titleWidth={"lg:!max-w-[650px]"}
+      />
+    </>
+  );
+};
 
-export default Team
+export default Team;
