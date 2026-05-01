7ec5cfa93745227d5a7a426d5340ad1cb18e28e8
basudevkumar <jhulonkumar40@gmail.com>
2026-05-01 18:17:59 +0600

    feat: in ServicesDetails pages added InnerPageHero resuable component

--- src/app/(pages)/services/[slug]/page.jsx
+++ src/app/(pages)/services/[slug]/page.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import InnerPageHero from "@/components/ui/InnerPageHero";
+import React from "react";
 
 const ServicesDetails = () => {
   return (
-    <div>ServicesDetails</div>
-  )
-}
+    <>
+      <InnerPageHero />
+    </>
+  );
+};
 
-export default ServicesDetails
+export default ServicesDetails;
