c1b71cf5fcebbe3f87cb4a32861494aef9a145c5
basudevkumar <jhulonkumar40@gmail.com>
2026-05-03 13:37:39 +0600

    feat: added ServiceDescription component in servicedetails page

--- src/components/helper/iconProvider.jsx
+++ src/components/helper/iconProvider.jsx
@@ -4,6 +4,8 @@ import { FiPlus } from "react-icons/fi";
 import { RxTriangleRight } from "react-icons/rx";
 import { RxTriangleLeft } from "react-icons/rx";
 import { GiPlainSquare } from "react-icons/gi";
+import { GoDotFill } from "react-icons/go";
+
 
 export const allIcons = {
   star: <IoStarSharp />,
@@ -12,4 +14,5 @@ export const allIcons = {
   triangleLeft: <RxTriangleLeft />,
   triangleRight: <RxTriangleRight />,
   square: <GiPlainSquare />,
+  dot: <GoDotFill />,
 };
